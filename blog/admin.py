c6968bc5315af1200697961bf75806d4b7db83cd
ibrah-55 <karikuibrahim55@gmail.com>
2021-03-03 09:13:18 +0300

    created models and configured the urls

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Post
+from django.urls import path, include
 
-
-admin.site.register(Post)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+]
